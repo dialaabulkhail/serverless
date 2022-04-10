26993449923a84ebd4ffc1d02923ea3bf0832907
dialaabulkhail <diala.sh.98@gmail.com>
2022-04-10 18:49:51 +0300

    double

--- api/double.py
+++ api/double.py
@@ -7,8 +7,7 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    x = [i*2 for i in range(50)]
-    self.wfile.write(x.encode())
+    self.wfile.write([i*2 for i in range(50)].encode())
     return
 
 
