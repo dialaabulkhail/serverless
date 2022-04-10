56e29b945eb9b5a0469ecfd4060609fe7e62fd58
dialaabulkhail <diala.sh.98@gmail.com>
2022-04-10 15:48:48 +0300

    test

--- api/test.py
+++ api/test.py
@@ -1,19 +1,16 @@
 from http.server import BaseHTTPRequestHandler
 
 
-def ran():
-    return "test"
-
 class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
+    def ran():
+        return "test"
+    ran()
     self.wfile.write(ran().encode())
     return
 
-  def ran(n):
-    x = [i for i in range(n)]
-    return x
 
