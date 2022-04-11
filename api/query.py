045fe12b5907bcf5bdbbd4fad9389f79d7bff299
dialaabulkhail <diala.sh.98@gmail.com>
2022-04-11 14:32:03 +0300

    test

--- api/query.py
+++ api/query.py
@@ -1,12 +1,23 @@
 from http.server import BaseHTTPRequestHandler
-from datetime import datetime
-
+from urllib import parse
 
 class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
+    url_path = self.path
+    url_components = parse.urlparse(url_path)
+    query_string_list = parse.parse_qsl(url_components.query)
+    dic = dict(query_string_list)
+
+    name = dic.get("name")
+
+    if name:
+        message = f"Hi {name}"
+    else:
+        message = "Hi"
+
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.wfile.write(str(datetime.now().strftime('DATE: ' + '%Y-%m-%d ' + '\n' 'TIME: ' + '%H:%M:%S')).encode())
-    return 
+    self.wfile.write(message.encode())
+    return
