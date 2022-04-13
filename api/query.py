519b629069f41a4dcf7fc5e5b0d09a7b3d9eb0cc
dialaabulkhail <diala.sh.98@gmail.com>
2022-04-13 15:57:21 +0300

    dictionary

--- api/query.py
+++ api/query.py
@@ -2,36 +2,6 @@ from http.server import BaseHTTPRequestHandler
 from tkinter.filedialog import dialogstates
 from urllib import parse
 
-def user():
-  users = [
-  {
-    "name": "Diala",
-    "age" : 23,
-    "education": "Civil Engineering",
-    "Hometown": "Amman"
-  },
-  {
-    "name": "Hala",
-    "age" : 16,
-    "education": "high school",
-    "Hometown": "Amman"
-  },
-  {
-    "name": "Jawad",
-    "age" : 18,
-    "education": "high school",
-    "Hometown": "Amman"
-  },
-  {
-    "name": "Jude",
-    "age" : 21,
-    "education": "medicine",
-    "Hometown": "Alexandria"
-  }]
-
-  return users
-
-
 
 class handler(BaseHTTPRequestHandler):
 
@@ -43,23 +13,14 @@ class handler(BaseHTTPRequestHandler):
     dic = dict(query_string_list)
 
     name = dic.get("name")
-    users = user()
 
-    for i in users:
-      if i["name"] == name:
-        message = f"Data of {name} is: {i}"
-        return message
-  
-
+    if name:
+        message = f"Hi {name}"
+    else:
+        message = "Hi"
 
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
     self.wfile.write(message.encode())
     return
-
-
-if __name__ == "__main__":
-
-      
-
