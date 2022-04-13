da99eb964befc52963349c5ff5566a0adfa1db68
dialaabulkhail <diala.sh.98@gmail.com>
2022-04-13 15:47:15 +0300

    dictionary

--- api/query.py
+++ api/query.py
@@ -1,7 +1,40 @@
 from http.server import BaseHTTPRequestHandler
+from tkinter.filedialog import dialogstates
 from urllib import parse
 
+def user():
+  users = [
+  {
+    "name": "Diala",
+    "age" : 23,
+    "education": "Civil Engineering",
+    "Hometown": "Amman"
+  },
+  {
+    "name": "Hala",
+    "age" : 16,
+    "education": "high school",
+    "Hometown": "Amman"
+  },
+  {
+    "name": "Jawad",
+    "age" : 18,
+    "education": "high school",
+    "Hometown": "Amman"
+  },
+  {
+    "name": "Jude",
+    "age" : 21,
+    "education": "medicine",
+    "Hometown": "Alexandria"
+  }]
+
+  return users
+
+
+
 class handler(BaseHTTPRequestHandler):
+
 
   def do_GET(self):
     url_path = self.path
@@ -10,11 +43,16 @@ class handler(BaseHTTPRequestHandler):
     dic = dict(query_string_list)
 
     name = dic.get("name")
+    users = user()
 
-    if name:
-        message = f"Hi {name}"
+    for i in users:
+      if i["name"] == name:
+        message = f"Data of {name} is: {i}"
+        return message
+    
     else:
-        message = "Hi"
+        message = "Hello, Welcome {name}"
+
 
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
